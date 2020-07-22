c3009e054587d1aeb99e1401018a6e8eb2f02f24
qvntm <32958247+qvntm@users.noreply.github.com>
2020-07-22 14:49:44 +0900

    Simple copy and paste attack

---
+++ id0-rsa.pub/26_Cut_and_Paste_Attack_On_AES-ECB/solve.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo by getting first 16 bytes of first 2 message and last 16 bytes of the last message, we can forge
+echo "'Deposit amount: ' + 'One million doll' + 'ars'"
+echo "so the answer is 5797791557579e322e619f12b0ccdee8b1e952572d6b8e00b626be86552376e2fdf526bd557b045bce65a3b3e300b55e"
