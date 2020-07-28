32a276f18255cb024e5ffe09bf49fabc1d1c3608
qvntm <32958247+qvntm@users.noreply.github.com>
2020-07-28 18:00:09 +0900

    Brute force using john the ripper

---
+++ id0-rsa.pub/41_Slightly_harder_passwords/decrypt.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import crypt
+import os
+
+print("Example hashes:")
+print("SHA256 :", crypt.crypt("hi", crypt.mksalt(crypt.METHOD_SHA256)))
+print("CRYPT  :", crypt.crypt("hi", crypt.mksalt(crypt.METHOD_CRYPT)))
+print("MD5    :", crypt.crypt("hi", crypt.mksalt(crypt.METHOD_MD5)))
+print("\n From here we can see that the given hash have the same length as CRYPT")
+
+# Since the salt of crypt algorithm has only 2 bytes, we can brute force by
+# applying the L33t rules to the dictionary in a reasonable time. But I will
+# just follow the hint and use John The Ripper to decrypt.
+# NOTE: I installed using apt install john and added to rules to /etc/john/john.conf
+
+os.system("john --format=crypt hashed.txt")
